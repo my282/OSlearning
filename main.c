aec492503e65f7c676908fcd39fd92d7baa57985
my282 <fm282@DESKTOP-FLQPLN7.localdomain>
2025-11-24 16:32:34 +0900

    feat:add compile.sh

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <efi.h>
 #include <efilib.h>
 
-
-
+EFIAPI EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+    InitializeLib(ImageHandle, SystemTable);
+    Print(L"Hello, UEFI World!\n");
+    return EFI_SUCCESS;
+}
 
 
