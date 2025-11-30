b3e7b50aa177a4fe112ea39ea6b569406d0ae90f
my282 <myoumyou1228@gmail.com>
2025-11-30 22:24:37 +0900

    clean: set up dir and files for EDK2

--- img.sh
+++
@@ -1,7 +0,0 @@
-qemu-img create -f raw disk.img 200M
-mkfs.fat -n 'FAMOUSOS' -s 2 -f 2 -R 32 -F 32 disk.img
-mkdir -p mnt
-sudo mount -o loop disk.img mnt
-sudo mkdir -p mnt/EFI/BOOT
-sudo cp BOOTX64.EFI mnt/EFI/BOOT/BOOTX64.EFI
-sudo umount mnt
