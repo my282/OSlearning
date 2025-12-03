749984a08c51f03485d4776d5e08bf0c77b5fcaa
famous <68148363+my282@users.noreply.github.com>
2025-12-03 16:27:24 +0900

    無駄

--- FamOSPkg/HelloWorld/main.c
+++ FamOSPkg/HelloWorld/main.c
@@ -2,7 +2,7 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
 // clang-format on
-
+//俺はスマホからアクセスしてるぞ！ジョジョー
 EFIAPI EFI_STATUS UefiMain(EFI_HANDLE ImageHandle,
                            EFI_SYSTEM_TABLE *SystemTable) {
   Print(L"Hello, UEFI World!\n");
